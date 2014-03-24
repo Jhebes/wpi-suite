3423562a229c4a4f3add733c43cd054dc09f81dc
nakalamvokis <nkal123@gmail.com>
2014-03-24 18:45:21 -0400

    creates PlaningPokerRequirement

---
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/models/PlanningPokerRequirement.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package edu.wpi.cs.wpisuitetcw.modules.planningpoker.models;
+
+/**
+ * @author Nicholas Kalamvokis and Matt Suarez
+ * @date 3/24/2014
+ */
+
+public class PlanningPokerRequirement implements Model{
+
+	/**
+	 * 
+	 */
+	public PlanningPokerRequirement() {
+		
+		
+		
+		
+		// TODO Auto-generated constructor stub
+	}
+
+}
