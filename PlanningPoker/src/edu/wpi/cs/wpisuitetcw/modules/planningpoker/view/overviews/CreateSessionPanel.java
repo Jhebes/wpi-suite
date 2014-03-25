a13772809917b939501b9372b196aefa3474b08d
Your Name <remckenna@wpi.edu>
2014-03-24 21:29:58 -0400

    Adds a Name text field, Deadline text field, and List of reqs.

--- PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/overviews/CreateSessionPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/overviews/CreateSessionPanel.java
@@ -1,8 +1,10 @@
 package edu.wpi.cs.wpisuitetcw.modules.planningpoker.view.overviews;
 
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
+import javax.swing.JTextField;
 
 /**
  * 
@@ -27,6 +29,27 @@ public class CreateSessionPanel extends JSplitPane {
 			
 			//Creates a List view in the UI that displays the dummy list
 			JList<String> existingReqsList = new JList<String>(dummyReqs);
+			
+			//Creates a Name text field in the leftPane
+			leftPanel.add(new JLabel("Name"));
+			JTextField nameField = new JTextField(10);
+			leftPanel.add(nameField);
+			
+			//Creates a deadline text field in the leftPane
+			leftPanel.add(new JLabel("Deadline"));
+			JTextField deadlineField = new JTextField(10);
+			leftPanel.add(deadlineField);
+			
+			//Creates a list of Reqs for the session
+			leftPanel.add(new JLabel("Deadline"));
+			leftPanel.add(existingReqsList);
+			
+			
+			this.setRightComponent((rightPanel));
+			this.setLeftComponent(leftPanel);
+			this.setDividerLocation(180);
+			
+			
 
 	}
 }
