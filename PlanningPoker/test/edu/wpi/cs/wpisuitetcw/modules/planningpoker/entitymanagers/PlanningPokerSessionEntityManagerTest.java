a3f54abc4b5237cbae69dda9058d604f7c367100
Ian Adam Naval <ianonavy@gmail.com>
2014-03-25 18:11:42 -0400

    Adds tests that were not added in the last commit [ianaval, psover].

---
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetcw/modules/planningpoker/entitymanagers/PlanningPokerSessionEntityManagerTest.java
@@ -0,0 +1,74 @@
+package edu.wpi.cs.wpisuitetcw.modules.planningpoker.entitymanagers;
+/*******************************************************************************
+ * Copyright (c) 2013 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Andrew Hurle
+ *    Tyler Wack
+ ******************************************************************************/
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+import java.util.HashSet;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.wpi.cs.wpisuitetcw.modules.planningpoker.MockData;
+import edu.wpi.cs.wpisuitetcw.modules.planningpoker.models.PlanningPokerSession;
+import edu.wpi.cs.wpisuitetng.Session;
+import edu.wpi.cs.wpisuitetng.database.Data;
+import edu.wpi.cs.wpisuitetng.exceptions.WPISuiteException;
+import edu.wpi.cs.wpisuitetng.modules.core.models.Project;
+import edu.wpi.cs.wpisuitetng.modules.core.models.User;
+
+public class PlanningPokerSessionEntityManagerTest {
+
+	User bob;
+	Project testProject;
+	Session defaultSession;
+	
+	Data db;
+	PlanningPokerSessionEntityManager manager;
+	
+	PlanningPokerSession session;
+	
+	String mockSsid;
+	
+	@Before
+	public void setUp() throws Exception {
+		mockSsid = "abc123";
+		bob = new User("bob", "bob", "1234", 1);
+		testProject = new Project("test", "1");
+		defaultSession = new Session(bob, testProject, mockSsid);
+		
+		session = new PlanningPokerSession();
+		session.setID(1);
+		
+		db = new MockData(new HashSet<Object>());
+		db.save(session, testProject);
+		db.save(bob);
+		manager = new PlanningPokerSessionEntityManager(db);
+	}
+
+	@Test
+	public void testMakeEntity() throws WPISuiteException {
+		PlanningPokerSession created = manager.makeEntity(defaultSession, session.toJSON());
+		assertEquals(2, created.getID());
+		assertSame(testProject, created.getProject());
+	}
+	
+	@Test
+	public void testGetEntity() throws WPISuiteException {
+		PlanningPokerSession[] sessions = manager.getEntity(defaultSession, "1");
+		assertEquals(1, sessions[0].getID());
+		assertSame(testProject, sessions[0].getProject());
+	}
+}
