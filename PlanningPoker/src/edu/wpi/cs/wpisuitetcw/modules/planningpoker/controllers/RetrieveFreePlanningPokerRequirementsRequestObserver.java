be7fd5e3325829f8bbfd828ac6381ef380a32c51
Ian Adam Naval <ianonavy@gmail.com>
2014-03-30 14:25:19 -0400

    Adds documentation for RetrieveFreePlanningPokerRequirementsRequestObserver.

--- PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/controllers/RetrieveFreePlanningPokerRequirementsRequestObserver.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/controllers/RetrieveFreePlanningPokerRequirementsRequestObserver.java
@@ -16,46 +16,66 @@ import edu.wpi.cs.wpisuitetng.network.models.IRequest;
 import edu.wpi.cs.wpisuitetng.network.models.ResponseModel;
 
 /**
- * An observer for a request to retrieve all defects
+ * An observer for a request to retrieve planning poker requirements that have
+ * not been assigned to a planning poker session.
  */
-public class RetrieveFreePlanningPokerRequirementsRequestObserver implements RequestObserver {
+public class RetrieveFreePlanningPokerRequirementsRequestObserver implements
+		RequestObserver {
 
 	/** The controller managing the request */
 	protected RetrieveFreePlanningPokerRequirementsController controller;
 
 	/**
-	 * Construct the observer
+	 * Constructs the observer
+	 * 
 	 * @param controller
+	 *            The controller to update upon receiving the server response
 	 */
-	public RetrieveFreePlanningPokerRequirementsRequestObserver(RetrieveFreePlanningPokerRequirementsController controller) {
+	public RetrieveFreePlanningPokerRequirementsRequestObserver(
+			RetrieveFreePlanningPokerRequirementsController controller) {
 		this.controller = controller;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void responseSuccess(IRequest iReq) {
 		// cast observable to request
 		Request request = (Request) iReq;
-		
+
 		// get the response from the request
 		ResponseModel response = request.getResponse();
 
 		if (response.getStatusCode() == 200) {
-			controller.receivedData(PlanningPokerRequirement.fromJsonArray(response.getBody()));
-		}
-		else {
-			controller.errorReceivingData("Received " + iReq.getResponse().getStatusCode() + " error from server: " + iReq.getResponse().getStatusMessage());
+			controller.receivedData(PlanningPokerRequirement
+					.fromJsonArray(response.getBody()));
+		} else {
+			controller.errorReceivingData("Received "
+					+ iReq.getResponse().getStatusCode()
+					+ " error from server: "
+					+ iReq.getResponse().getStatusMessage());
 		}
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void responseError(IRequest iReq) {
 		// an error occurred
-		controller.errorReceivingData("Received " + iReq.getResponse().getStatusCode() + " error from server: " + iReq.getResponse().getStatusMessage());
+		controller.errorReceivingData("Received "
+				+ iReq.getResponse().getStatusCode() + " error from server: "
+				+ iReq.getResponse().getStatusMessage());
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void fail(IRequest iReq, Exception exception) {
 		// an error occurred
-		controller.errorReceivingData("Unable to complete request: " + exception.getMessage());
+		controller.errorReceivingData("Unable to complete request: "
+				+ exception.getMessage());
 	}
 }
