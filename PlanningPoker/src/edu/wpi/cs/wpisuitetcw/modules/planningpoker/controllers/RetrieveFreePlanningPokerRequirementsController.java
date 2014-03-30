5f795a9cfa9a4eecb3b45838b566a6b077382137
Ian Adam Naval <ianonavy@gmail.com>
2014-03-30 12:11:28 -0400

    Prevents data from being refreshed every time controller is instantiated.

--- PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/controllers/RetrieveFreePlanningPokerRequirementsController.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/controllers/RetrieveFreePlanningPokerRequirementsController.java
@@ -40,7 +40,6 @@ public class RetrieveFreePlanningPokerRequirementsController {
 	public RetrieveFreePlanningPokerRequirementsController(
 			CreateSessionPanel panel) {
 		this.panel = panel;
-		this.refreshData();
 	}
 
 	/**
