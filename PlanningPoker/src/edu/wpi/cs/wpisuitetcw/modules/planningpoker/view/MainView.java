7f0330895b1b198b91e3981f47778452b01b58ac
Troy Ling <troyling14@gmail.com>
2014-03-31 08:52:17 -0400

    disables NewSession panel when module is first loaded. Troy

--- PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/MainView.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/MainView.java
@@ -26,25 +26,28 @@ public class MainView extends JTabbedPane {
 	public MainView() {
 		overivewPanel = new OverviewPanel();
 		this.addTab("Overview", overivewPanel);
-		
-		createSessionPanel = new CreateSessionPanel();
-		this.addTab("New Session", createSessionPanel);
-		
+
 		sessionInProgressPanel = new SessionInProgressPanel();
 		this.addTab("In Progress", sessionInProgressPanel);
 	}
-	
+
 	/**
 	 * Overridden insertTab function to add the closable tab element.
 	 * 
-	 * @param title	Title of the tab
-	 * @param icon	Icon for the tab
-	 * @param component	The tab
-	 * @param tip	Showing mouse tip when hovering over tab
-	 * @param index	Location of the tab
+	 * @param title
+	 *            Title of the tab
+	 * @param icon
+	 *            Icon for the tab
+	 * @param component
+	 *            The tab
+	 * @param tip
+	 *            Showing mouse tip when hovering over tab
+	 * @param index
+	 *            Location of the tab
 	 */
 	@Override
-	public void insertTab(String title, Icon icon, Component component, String tip, int index) {
+	public void insertTab(String title, Icon icon, Component component,
+			String tip, int index) {
 		super.insertTab(title, icon, component, tip, index);
 		if (!(component instanceof OverviewPanel)) {
 			setTabComponentAt(index, new ClosableTabComponent(this));
