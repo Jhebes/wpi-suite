621f454dbbc4da3da030bbb18de19c8dda186517
nakalamvokis <nkal123@gmail.com>
2014-03-30 20:05:51 -0400

    nakalamvokis mattjsuarez implements PlanningPokerVote and PlanningPokerVoteEntityManager

--- PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/entitymanagers/PlanningPokerVoteEntityManager.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/entitymanagers/PlanningPokerVoteEntityManager.java
@@ -12,6 +12,7 @@ import edu.wpi.cs.wpisuitetng.database.Data;
 import edu.wpi.cs.wpisuitetng.exceptions.BadRequestException;
 import edu.wpi.cs.wpisuitetng.exceptions.ConflictException;
 import edu.wpi.cs.wpisuitetng.exceptions.NotFoundException;
+import edu.wpi.cs.wpisuitetng.exceptions.NotImplementedException;
 import edu.wpi.cs.wpisuitetng.exceptions.WPISuiteException;
 import edu.wpi.cs.wpisuitetng.modules.EntityManager;
 import edu.wpi.cs.wpisuitetng.modules.Model;
@@ -19,12 +20,12 @@ import edu.wpi.cs.wpisuitetng.modules.Model;
 
 /**
  * @author Nick Kalamvokis and Matt Suarez
- *
+ * 
  */
 
 public class PlanningPokerVoteEntityManager implements
-EntityManager<PlanningPokerVote> {
-	
+		EntityManager<PlanningPokerVote> {
+
 	/** The database */
 	Data db;
 
@@ -40,136 +41,154 @@ EntityManager<PlanningPokerVote> {
 	public PlanningPokerVoteEntityManager(Data db) {
 		this.db = db;
 	}
-	
-	
+
 	// Need review, not sure what to do with content and saving to database
-public PlanningPokerVote makeEntity(Session s, String content) throws BadRequestException, ConflictException, WPISuiteException {
-	
-	PlanningPokerVote newVote = new PlanningPokerVote();
-	
-	// try to save the vote to the database
-	// throw WPISuiteException if this doesn't work
-	if (!db.save(newVote, s.getProject())) {
-		throw new WPISuiteException();
+	public PlanningPokerVote makeEntity(Session s, String content)
+			throws BadRequestException, ConflictException, WPISuiteException {
+		
+		final PlanningPokerVote newVote = PlanningPokerVote.fromJson(content);
+		
+		// try to save the vote to the database throw WPISuiteException if this doesn't work
+		if (!db.save(newVote, s.getProject())) {
+			throw new WPISuiteException();
+		}
+
+		// return the new vote
+		return newVote;
 	}
-	
-	
-	// return the new vote
-	return newVote;
-}
-	
-	/* Retrieve */	
+
+	/* Retrieve */
 	/**
 	 * Retrieves the PlanningPokerVote with the given unique identifier, id.
-	 * @param id	the unique identifier value
-	 * @return	the PlanningPokerVote with the given ID
-	 * @throws NotFoundException if entity does not exist
+	 * 
+	 * @param id
+	 *            the unique identifier value
+	 * @return the PlanningPokerVote with the given ID
+	 * @throws NotFoundException
+	 *             if entity does not exist
 	 */
-	public PlanningPokerVote[] getEntity(Session s, String id) throws NotFoundException, WPISuiteException;
-	
+	@Override
+	public PlanningPokerVote[] getEntity(Session s, String id)
+			throws NotFoundException {
+		final int intID = Integer.parseInt(id);
+		if (intID < 1) {
+			throw new NotFoundException();
+		}
+		PlanningPokerVote[] votes = null;
+		try {
+			votes = db.retrieve(PlanningPokerVote.class, "id", intID, s.getProject())
+					.toArray(new PlanningPokerVote[0]);
+		} catch (WPISuiteException e) {
+			e.printStackTrace();
+		}
+		if (votes.length < 1 || votes[0] == null) {
+			throw new NotFoundException();
+		}
+		return votes;
+	}
+
 	/**
 	 * Retrieves all entities of Model class PlanningPokerVote
-	 * @return	an ArrayList<PlanningPokerVote> with all instances of PlanningPokerVote
+	 * 
+	 * @return an ArrayList<PlanningPokerVote> with all instances of
+	 *         PlanningPokerVote
 	 */
-	public PlanningPokerVote[] getAll(Session s) throws WPISuiteException{
+	@Override
+	public PlanningPokerVote[] getAll(Session s) throws WPISuiteException {
 		List<Model> votes = db.retrieveAll(new PlanningPokerVote(),
 				s.getProject());
-		
+
 		// return the list of votes as an array
 		return votes.toArray(new PlanningPokerVote[0]);
 	}
-	
-	
+
 	/* Update */
 	/**
 	 * 
-	 * @param s the session of the User executing this action
-	 * @param content - JSON representation of the model updates
+	 * @param s
+	 *            the session of the User executing this action
+	 * @param content
+	 *            - JSON representation of the model updates
 	 * @return the updated model
 	 * @throws WPISuiteException
 	 */
-	public PlanningPokerVote update(Session s, String content) throws WPISuiteException{
+	@Override
+	public PlanningPokerVote update(Session s, String content)
+			throws WPISuiteException {
 		throw new WPISuiteException();
 	}
-	
+
 	/**
 	 * Saves the given model of class T to the database
-	 * @param model	the Model to update.
+	 * 
+	 * @param model
+	 *            the Model to update.
 	 */
-	public void save(Session s, PlanningPokerVote model) throws WPISuiteException{
-		db.save(model);
+	@Override
+	public void save(Session s, PlanningPokerVote model)
+			throws WPISuiteException {
+		db.save(model, s.getProject());
 	}
-	
-	/* Delete */  // NEEDS REVIEW
+
+	/* Delete */
 	/**
 	 * Deletes the entity with the given unique identifier, id.
-	 * @param id	the unique identifier for the entity
+	 * 
+	 * @param id
+	 *            the unique identifier for the entity
 	 */
-	public boolean deleteEntity(Session s, String id) throws WPISuiteException{
-		// db.delete( a PlanningPokerVote);
+	@Override
+	public boolean deleteEntity(Session s, String id) throws WPISuiteException {
+		return (db.delete(getEntity(s, id)[0]) != null);
 	}
-	
-	/* Advanced Get*/
+
+	/* Advanced Get */
 	/**
-	 * This method is accessed through the API by a GET request to the AdvancedAPI
-	 * @param s	The session that requested this action
-	 * @param args[] A String array containing the entire path of the GET that initiated this action
+	 * {@inheritDoc}
 	 */
-	public String advancedGet(Session s, String[] args) throws WPISuiteException{
-		return null;
+	@Override
+	public String advancedGet(Session s, String[] args)
+			throws NotImplementedException {
+		throw new NotImplementedException();
 	}
-	
+
 	/**
 	 * Deletes all entities of Model class T
+	 * @param s Current session
 	 */
-	public void deleteAll(Session s) throws WPISuiteException{
-		return null;
+	@Override
+	public void deleteAll(Session s) throws WPISuiteException {
+		db.deleteAll(new PlanningPokerVote(), s.getProject());
 	}
-	
+
 	/* Utility Methods */
 	/**
 	 * 
-	 * @return	the number of records of this Manager's Model class T
+	 * @return the number of records of this Manager's Model class T
 	 */
-	public int Count() throws WPISuiteException{
+	@Override
+	public int Count() throws WPISuiteException {
 		return db.retrieveAll(new PlanningPokerVote()).size();
 	}
 
-	/* Advanced Put*/
+	/* Advanced Put */
 	/**
-	 * **************
-	 * A note about the content body.  Must not contain any line breaks.  Only the first line is passed 
-	 * to this function.                                            
-	 * **************
-	 * This method is accessed through the API by a PUT request to the AdvancedAPI
-	 * @param s	The session that requested this action
-	 * @param args[] A String array containing the entire path of the PUT that initiated this action
-	 * @param content The content body of the request
+	 * {@inheritDoc}
 	 */
-	public String advancedPut(Session s, String[] args, String content) throws WPISuiteException{
-		return null;
+	@Override
+	public String advancedPut(Session s, String[] args, String content)
+			throws NotImplementedException {
+		throw new NotImplementedException();
 	}
 
-	/*Advanced Post*/
+	/* Advanced Post */
 	/**
-	 * 
-	 * **********************
-	 * A note about advanced post.  the content body should not contain any line breaks.
-	 * only the first line will be passed through to the function.
-	 * **********************
-	 * This method is similar to the Advanced Put method, except that where ADvanced Put recieves the entire path
-	 * as a String array, Advanced Post only receives the third path argument, for example API/advanced/module/model/argument
-	 * The reasoning behind this was to allow module developers who required more methods a way to multiplex this 
-	 * one call into multiple different functions.  By using the argument as a key in a switch statement or if else block, 
-	 * they can process the content payload through an unlimited number of functions.
-	 * 
-	 * @param s - The session that requested this action
-	 * @param string - A path identifier [/module/model/identifier]
-	 * @param content - The String payload of the request
-	 * @return - a String to be sent back as the body of the request
+	 * {@inheritDoc}
 	 */
-	public String advancedPost(Session s, String string, String content) throws WPISuiteException{
-		return null;
+	@Override
+	public String advancedPost(Session s, String string, String content)
+			throws NotImplementedException {
+		throw new NotImplementedException();
 	}
 
 }
--- PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/models/PlanningPokerVote.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/models/PlanningPokerVote.java
@@ -3,81 +3,113 @@
  */
 package edu.wpi.cs.wpisuitetcw.modules.planningpoker.models;
 
-import edu.wpi.cs.wpisuitetng.Permission;
-import edu.wpi.cs.wpisuitetng.modules.core.models.Project;
-import edu.wpi.cs.wpisuitetng.modules.core.models.User;
+import com.google.gson.Gson;
+
 import edu.wpi.cs.wpisuitetng.modules.AbstractModel;
+import edu.wpi.cs.wpisuitetng.modules.core.models.User;
 
 /**
  * @author Nick Kalamvokis and Matt Suarez
- *
+ * 
  */
 public class PlanningPokerVote extends AbstractModel {
 
 	private int id;
 	private PlanningPokerSession session;
 	private PlanningPokerRequirement requirement;
-	
-	/* We need a type User
-	 *private User user;
-	 */
-	
-	/* We need a type Card
-	 * private Card card;
-	 */
-	
-	/**
-	 * 
-	 */
-	public PlanningPokerVote() {
+	private User user;
+	private int cardValue;
+
+	public PlanningPokerVote(PlanningPokerSession pps,
+			PlanningPokerRequirement ppr, User u, int val) {
+		this.session = pps;
+		this.requirement = ppr;
+		this.user = u;
+		this.cardValue = val;
 	}
-	
-	public PlanningPokerVote(int id, PlanningPokerSession pps, PlanningPokerRequirement ppr, User u, Card c) {
+
+	public PlanningPokerVote(int id, PlanningPokerSession pps,
+			PlanningPokerRequirement ppr, User u, int val) {
 		this.id = id;
 		this.session = pps;
 		this.requirement = ppr;
 		this.user = u;
-		this.card = c;
+		this.cardValue = val;
+	}
+
+	/* database interaction */
+	@Override
+	public void save() {
+	}
+
+	@Override
+	public void delete() {
+	}
+
+	/* serializing */// NEED HELP
+
+	/**
+	 * toJSON : serializing this Model's contents into a JSON/GSON string
+	 * 
+	 * @return A string containing the serialized JSON representation of this
+	 *         Model.
+	 */
+	@Override
+	public String toJSON() {
+		return new Gson().toJson(this, PlanningPokerVote.class);
 	}
 	
-	/* database interaction */ // NEED HELP
-	public void save();
-	public void delete();
-	
-	/* serializing */  // NEED HELP
-	
-	/** toJSON : serializing this Model's contents into a JSON/GSON string
-	 * @return	A string containing the serialized JSON representation of this Model.
-	 */
-	public String toJSON();
-	
-	/* deserializing */
-	
-	
-	
-	
-	/* Built-in overrides/overloads */
-	
-	/** toString : enforce an override. May simply call serializeToJSON.
-	 * @return	The string representation of this Model
-	 */
-	public String toString() {
-		return ("ID: " + this.id + "   Session ID: " + this.session.getID() + "   Requirement ID: " 
-				+ this.requirement.getId() + "   User ID: " + this.user.getId() + "   Card Info: " 
-				+ this.card.toString());
-	}
 	
 	/**
-	 * identify: true if the argument o is equal this object's unique identifier or this object
-	 * this method was created for use with the mock database
+	 * Convert from JSON back to a Planning Poker Session
 	 * 
-	 * implementations overriding this method should check if o is either a unique identifier, or an instance of this class
-	 * if o is an instance of this class, this method should check if it contains the same unique identifier
-	 * 
-	 * @param o - a unique identifier belonging to an object
-	 * @return true if the o is equal to this Model's unique identifier, else false
+	 * @param Serialized
+	 *            JSON String that encodes to a Session Model
+	 * @return the PlanningPokerSession contained in the given JSON
 	 */
-	public Boolean identify(Object o) {
-		return this.id == (Integer) o;
+	public static PlanningPokerVote fromJson(String json) {
+		final Gson parser = new Gson();
+		return parser.fromJson(json, PlanningPokerVote.class);
 	}
+
+	/**
+	 * toString : enforce an override. May simply call serializeToJSON.
+	 * 
+	 * @return The string representation of this Model
+	 */
+	@Override
+	public String toString() {
+		return ("ID: " + this.id + "   Session ID: " + this.session.getID()
+				+ "   Requirement ID: " + this.requirement.getId()
+				+ "   User ID: " + this.user.getIdNum() + "   Card Info: " + this.cardValue);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Boolean identify(Object o) {
+		return ((Integer) o).equals(this.id);
+	}
+	
+
+	public PlanningPokerVote() {
+	}
+
+	public int getID() {
+		return id;
+	}
+
+	public void setID(int id) {
+		this.id = id;
+	}
+
+	public int getCardValue() {
+		return cardValue;
+	}
+
+	public void setCardValue(int cardValue) {
+		this.cardValue = cardValue;
+	}
+	
 }
