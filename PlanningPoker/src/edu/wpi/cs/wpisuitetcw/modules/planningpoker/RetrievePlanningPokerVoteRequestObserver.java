d045ce830a309c41e1945835d0be1ccfbd1ddef9
nakalamvokis <nkal123@gmail.com>
2014-03-31 19:10:49 -0400

    nakalamvokis and mattjsuarez adds vote controller and observer

---
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/RetrievePlanningPokerVoteRequestObserver.java
@@ -0,0 +1,64 @@
+/**
+ * @author Nick Kalamvokis and Matt Suarez
+ */
+
+package edu.wpi.cs.wpisuitetcw.modules.planningpoker;
+
+import edu.wpi.cs.wpisuitetcw.modules.planningpoker.models.PlanningPokerVote;
+import edu.wpi.cs.wpisuitetng.modules.defecttracker.search.controllers.RetrieveDefectController;
+import edu.wpi.cs.wpisuitetng.network.Request;
+import edu.wpi.cs.wpisuitetng.network.RequestObserver;
+import edu.wpi.cs.wpisuitetng.network.models.IRequest;
+import edu.wpi.cs.wpisuitetng.network.models.ResponseModel;
+
+/**
+ * An observer for a request to retrieve a defect with the provided id
+ */
+public class RetrievePlanningPokerVoteRequestObserver implements RequestObserver {
+
+	/** The retrieve defect controller using this observer */
+	protected RetrieveDefectController controller;
+
+	/**
+	 * Construct a new observer
+	 * @param controller the controller managing the request
+	 */
+	public RetrievePlanningPokerVoteRequestObserver(RetrieveDefectController controller) {
+		this.controller = controller;
+	}
+
+	@Override
+	public void responseSuccess(IRequest iReq) {
+		// cast observable to a Request
+		Request request = (Request) iReq;
+
+		// get the response from the request
+		ResponseModel response = request.getResponse();
+
+		// check the response code of the request
+		if (response.getStatusCode() != 200) {
+			controller.errorRetrievingDefect("Received " + iReq.getResponse().getStatusCode() + " error from server: " + iReq.getResponse().getStatusMessage());
+			return;
+		}
+
+		// parse the PlanningPokerVote received from the core
+		PlanningPokerVote[] defects = PlanningPokerVote.fromJSONArray(response.getBody());
+		if (defects.length > 0 && defects[0] != null) {
+			controller.showPlanningPokerVote(defects[0]);
+		}
+		else {
+			controller.errorRetrievingPlanningPokerVote("No defects received.");
+		}
+	}
+
+	@Override
+	public void responseError(IRequest iReq) {
+		controller.errorRetrievingPlanningPokerVote("Received " + iReq.getResponse().getStatusCode() + " error from server: " + iReq.getResponse().getStatusMessage());
+	}
+
+	@Override
+	public void fail(IRequest iReq, Exception exception) {
+		// TODO deal with exception
+		controller.errorRetrievingPlanningPokerVote("Unable to complete request: " + exception.getMessage());
+	}
+}
