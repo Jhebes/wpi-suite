770ff63160c8b5fe1ce07faf788dc3e876627bf3
Hoang Ngo <hoangngo.dev@gmail.com>
2014-04-01 05:05:41 -0400

    Re-format the code

--- PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/controllers/GetClosedSessionsRequestObserver.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/controllers/GetClosedSessionsRequestObserver.java
@@ -1,6 +1,16 @@
-/**
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2013 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Andrew Hurle
+ *    Chris Casola
+ ******************************************************************************/
+
 package edu.wpi.cs.wpisuitetcw.modules.planningpoker.controllers;
 
 import java.util.ArrayList;
@@ -11,23 +21,36 @@ import edu.wpi.cs.wpisuitetng.network.models.IRequest;
 
 /**
  * @author Hoang Ngo
- *
+ * 
  */
 public class GetClosedSessionsRequestObserver implements RequestObserver {
 	private final GetClosedSessionsController controller;
-	
-	public GetClosedSessionsRequestObserver(GetClosedSessionsController controller) {
+
+	/**
+	 * Construct a GetClosedSessionsRequestObserver for a given controller
+	 * 
+	 * @param controller
+	 */
+	public GetClosedSessionsRequestObserver(
+			GetClosedSessionsController controller) {
 		this.controller = controller;
 	}
-	
-	/* (non-Javadoc)
-	 * @see edu.wpi.cs.wpisuitetng.network.RequestObserver#responseSuccess(edu.wpi.cs.wpisuitetng.network.models.IRequest)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * edu.wpi.cs.wpisuitetng.network.RequestObserver#responseSuccess(edu.wpi
+	 * .cs.wpisuitetng.network.models.IRequest)
 	 */
 	@Override
 	public void responseSuccess(IRequest iReq) {
 		// Parse the sessions out of the response body
-		PlanningPokerSession[] sessions = PlanningPokerSession.fromJsonArray(iReq.getResponse().getBody());
-		
+		PlanningPokerSession[] sessions = PlanningPokerSession
+												.fromJsonArray(iReq
+																.getResponse()
+																.getBody());
+
 		// Filter the closed sessions using arrayList
 		ArrayList<PlanningPokerSession> tempClosedSessions = new ArrayList<PlanningPokerSession>();
 		for (int i = 0; i < sessions.length; i++) {
@@ -35,17 +58,22 @@ public class GetClosedSessionsRequestObserver implements RequestObserver {
 				tempClosedSessions.add(sessions[i]);
 			}
 		}
-		
+
 		// Convert the arrayList to array
-		PlanningPokerSession[] closedSessions = new PlanningPokerSession[tempClosedSessions.size()];
+		PlanningPokerSession[] closedSessions = 
+				new PlanningPokerSession[tempClosedSessions.size()];
 		closedSessions = tempClosedSessions.toArray(closedSessions);
-		
+
 		// Pass the session back to the controller
 		controller.receiveClosedSessions(closedSessions);
 	}
 
-	/* (non-Javadoc)
-	 * @see edu.wpi.cs.wpisuitetng.network.RequestObserver#responseError(edu.wpi.cs.wpisuitetng.network.models.IRequest)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * edu.wpi.cs.wpisuitetng.network.RequestObserver#responseError(edu.wpi.
+	 * cs.wpisuitetng.network.models.IRequest)
 	 */
 	@Override
 	public void responseError(IRequest iReq) {
@@ -53,8 +81,12 @@ public class GetClosedSessionsRequestObserver implements RequestObserver {
 		System.err.println("The request to get closed sessions failed.");
 	}
 
-	/* (non-Javadoc)
-	 * @see edu.wpi.cs.wpisuitetng.network.RequestObserver#fail(edu.wpi.cs.wpisuitetng.network.models.IRequest, java.lang.Exception)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * edu.wpi.cs.wpisuitetng.network.RequestObserver#fail(edu.wpi.cs.wpisuitetng
+	 * .network.models.IRequest, java.lang.Exception)
 	 */
 	@Override
 	public void fail(IRequest iReq, Exception exception) {
--- PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/controllers/GetOpenSessionsRequestObserver.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/controllers/GetOpenSessionsRequestObserver.java
@@ -1,6 +1,16 @@
-/**
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2013 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Andrew Hurle
+ *    Chris Casola
+ ******************************************************************************/
+
 package edu.wpi.cs.wpisuitetcw.modules.planningpoker.controllers;
 
 import java.util.ArrayList;
@@ -10,27 +20,38 @@ import edu.wpi.cs.wpisuitetng.network.RequestObserver;
 import edu.wpi.cs.wpisuitetng.network.models.IRequest;
 
 /**
- * This observer handles responses to requests for all
- * open planning poker sessions
+ * This observer handles responses to requests for all open planning poker
+ * sessions
  * 
  * @author Hoang Ngo
- *
+ * 
  */
 public class GetOpenSessionsRequestObserver implements RequestObserver {
 	private final GetOpenSessionsController controller;
-	
+
+	/**
+	 * Construct a GetOpenSessionsRequestObserver for the given controller
+	 * @param controller a GetOpenSessionsController
+	 */
 	public GetOpenSessionsRequestObserver(GetOpenSessionsController controller) {
 		this.controller = controller;
 	}
-	
-	/* (non-Javadoc)
-	 * @see edu.wpi.cs.wpisuitetng.network.RequestObserver#responseSuccess(edu.wpi.cs.wpisuitetng.network.models.IRequest)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * edu.wpi.cs.wpisuitetng.network.RequestObserver#responseSuccess(edu.wpi
+	 * .cs.wpisuitetng.network.models.IRequest)
 	 */
 	@Override
 	public void responseSuccess(IRequest iReq) {
 		// Parse all sessions out of the response body
-		PlanningPokerSession[] sessions = PlanningPokerSession.fromJsonArray(iReq.getResponse().getBody());
-		
+		PlanningPokerSession[] sessions = PlanningPokerSession
+											.fromJsonArray(iReq
+															.getResponse()
+															.getBody());
+
 		// Filter the open sessions
 		ArrayList<PlanningPokerSession> tempOpenSessions = new ArrayList<PlanningPokerSession>();
 		for (int i = 0; i < sessions.length; i++) {
@@ -38,17 +59,22 @@ public class GetOpenSessionsRequestObserver implements RequestObserver {
 				tempOpenSessions.add(sessions[i]);
 			}
 		}
-		
+
 		// Convert the open sessions arrayList to array
-		PlanningPokerSession[] openSessions = new PlanningPokerSession[tempOpenSessions.size()];
+		PlanningPokerSession[] openSessions = 
+				new PlanningPokerSession[tempOpenSessions.size()];
 		openSessions = tempOpenSessions.toArray(openSessions);
-		
+
 		// Pass the sessions back to the controller
 		controller.receiveOpenSessions(openSessions);
 	}
 
-	/* (non-Javadoc)
-	 * @see edu.wpi.cs.wpisuitetng.network.RequestObserver#responseError(edu.wpi.cs.wpisuitetng.network.models.IRequest)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * edu.wpi.cs.wpisuitetng.network.RequestObserver#responseError(edu.wpi.
+	 * cs.wpisuitetng.network.models.IRequest)
 	 */
 	@Override
 	public void responseError(IRequest iReq) {
@@ -56,8 +82,12 @@ public class GetOpenSessionsRequestObserver implements RequestObserver {
 		System.err.println("The request to get open sessions failed");
 	}
 
-	/* (non-Javadoc)
-	 * @see edu.wpi.cs.wpisuitetng.network.RequestObserver#fail(edu.wpi.cs.wpisuitetng.network.models.IRequest, java.lang.Exception)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * edu.wpi.cs.wpisuitetng.network.RequestObserver#fail(edu.wpi.cs.wpisuitetng
+	 * .network.models.IRequest, java.lang.Exception)
 	 */
 	@Override
 	public void fail(IRequest iReq, Exception exception) {
