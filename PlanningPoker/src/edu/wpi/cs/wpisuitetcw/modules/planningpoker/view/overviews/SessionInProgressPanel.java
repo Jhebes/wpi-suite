5e8325af9ea54a014d611279987bdefcd6e57ee8
Josh Hebert <jahebert@wpi.edu>
2014-03-31 20:01:17 -0400

    Corrects formatting to be in compliance with WPI Suite

--- PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/overviews/SessionInProgressPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/overviews/SessionInProgressPanel.java
@@ -38,64 +38,65 @@ public class SessionInProgressPanel extends JSplitPane {
 		JPanel LeftPanel = new JPanel();
 		LeftPanel.setBackground(SystemColor.menu);
 		LeftPanel.setLayout(new BoxLayout(LeftPanel, BoxLayout.Y_AXIS));
-		
+
 		Component verticalStrut = Box.createVerticalStrut(10);
 		LeftPanel.add(verticalStrut);
-		
+
 		JLabel lblSessionInfo = new JLabel("Session Info:");
 		lblSessionInfo.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		LeftPanel.add(lblSessionInfo);
-		
+
 		Component verticalStrut2 = Box.createVerticalStrut(20);
 		LeftPanel.add(verticalStrut2);
-		
+
 		JLabel lblName = new JLabel("Name:");
 		lblName.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		LeftPanel.add(lblName);
-		
+
 		JLabel name = new JLabel("Planning Poker", JLabel.CENTER);
 		LeftPanel.add(name);
-		
+
 		Component verticalStrut3 = Box.createVerticalStrut(20);
 		LeftPanel.add(verticalStrut3);
-		
+
 		JLabel lblDate = new JLabel("Session Ends:");
 		lblDate.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		LeftPanel.add(lblDate);	
-		
+		LeftPanel.add(lblDate);
+
 		JLabel date = new JLabel("12/13/14", JLabel.CENTER);
 		LeftPanel.add(date);
 
 		JSplitPane RightPanel = new JSplitPane();
 		RightPanel.setBackground(Color.WHITE);
 		RightPanel.setLayout(new BoxLayout(RightPanel, BoxLayout.X_AXIS));
-		
+
 		JPanel requirementsPanel = new JPanel();
-		requirementsPanel.setLayout(new BoxLayout(requirementsPanel, BoxLayout.X_AXIS));
-		
+		requirementsPanel.setLayout(new BoxLayout(requirementsPanel,
+				BoxLayout.X_AXIS));
+
 		JList requirementsList = new JList();
 		RightPanel.setLeftComponent(requirementsList);
-		
+
 		JPanel requiementsDetailPanel = new JPanel();
 		RightPanel.setRightComponent(requiementsDetailPanel);
 		requiementsDetailPanel.setLayout(new BorderLayout(0, 0));
-		
+
 		JLabel lblNewLabel = new JLabel("Requirement Detail:");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		lblNewLabel.setVerticalAlignment(SwingConstants.TOP);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		requiementsDetailPanel.add(lblNewLabel);
-		
+
 		JPanel panel = new JPanel();
 		requiementsDetailPanel.add(panel, BorderLayout.SOUTH);
-		
+
 		JLabel lblEstimate = new JLabel("Estimate:");
 		panel.add(lblEstimate);
-		
+
 		textField = new JTextField();
 		panel.add(textField);
 		textField.setColumns(10);
-		
+
 		JButton btnSubmit = new JButton("Submit");
 		panel.add(btnSubmit);
 
