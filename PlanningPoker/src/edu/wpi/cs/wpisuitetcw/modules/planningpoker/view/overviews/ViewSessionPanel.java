5d7a7c36dbb86eeb1f2ef93ead707a0513fcf484
Troy Ling <troyling14@gmail.com>
2014-04-01 22:31:07 -0400

    adds activate buttons and package view panel in viewSession panel. Troy

--- PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/overviews/ViewSessionPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/overviews/ViewSessionPanel.java
@@ -11,6 +11,7 @@ import javax.swing.JSplitPane;
 
 import edu.wpi.cs.wpisuitetcw.modules.planningpoker.view.overviews.viewSessionComp.ViewSessionBtnPanel;
 import edu.wpi.cs.wpisuitetcw.modules.planningpoker.view.overviews.viewSessionComp.ViewSessionInfoPanel;
+import edu.wpi.cs.wpisuitetcw.modules.planningpoker.view.overviews.viewSessionComp.ViewSessionPkgPanel;
 
 /**
  * @author troyling
@@ -19,6 +20,7 @@ import edu.wpi.cs.wpisuitetcw.modules.planningpoker.view.overviews.viewSessionCo
 public class ViewSessionPanel extends JSplitPane{
 	final private ViewSessionBtnPanel buttonPanel;
 	final private ViewSessionInfoPanel infoPanel;
+	final private ViewSessionPkgPanel pkgPanel;
 	
 	/**
 	 * Create a view session panel
@@ -26,13 +28,17 @@ public class ViewSessionPanel extends JSplitPane{
 	public ViewSessionPanel(String sessionName) {
 		this.infoPanel = new ViewSessionInfoPanel(this, sessionName);
 		this.buttonPanel = new ViewSessionBtnPanel(this);
+		this.pkgPanel = new ViewSessionPkgPanel(this);
 		
 		// delete these
 		infoPanel.setBorder(BorderFactory.createLineBorder(Color.GREEN));
 		buttonPanel.setBorder(BorderFactory.createLineBorder(Color.RED));
+		pkgPanel.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+		
+		JSplitPane contentPanel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, true, infoPanel, pkgPanel);
 		
 		this.setLayout(new BorderLayout());
-		this.add(infoPanel, BorderLayout.WEST);
+		this.add(contentPanel, BorderLayout.CENTER);
 		this.add(buttonPanel, BorderLayout.SOUTH);
 		
 	}
