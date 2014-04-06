Jhebes/wpi-suite

commit d099860f3b78ccef1facac987a1bf3703e75f5e6

changes sessionInProgress View

PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/overviews/SessionInProgressPanel.java
@@ -20,9 +20,11 @@ import edu.wpi.cs.wpisuitetcw.modules.planningpoker.controllers.AddVoteControlle
 import edu.wpi.cs.wpisuitetcw.modules.planningpoker.controllers.RetrieveAllPlanningPokerVoteController;
 import edu.wpi.cs.wpisuitetcw.modules.planningpoker.models.PlanningPokerRequirement;
 import edu.wpi.cs.wpisuitetcw.modules.planningpoker.models.PlanningPokerVote;
+import javax.swing.JTabbedPane;
+import java.awt.GridLayout;
+import javax.swing.JTree;
 
 public class SessionInProgressPanel extends JSplitPane {
-	private JTextField textField;
 
 	/**
 	 * Create the panel.
@@ -59,52 +61,50 @@ public class SessionInProgressPanel extends JSplitPane {
 		JLabel date = new JLabel("12/13/14", JLabel.CENTER);
 		LeftPanel.add(date);
 
-		JSplitPane RightPanel = new JSplitPane();
-		RightPanel.setBackground(Color.WHITE);
-		RightPanel.setLayout(new BoxLayout(RightPanel, BoxLayout.X_AXIS));
-
 		JPanel requirementsPanel = new JPanel();
 		requirementsPanel.setLayout(new BoxLayout(requirementsPanel,
 				BoxLayout.X_AXIS));
-
-		JList requirementsList = new JList();
-		RightPanel.setLeftComponent(requirementsList);
-
-		JPanel requiementsDetailPanel = new JPanel();
-		RightPanel.setRightComponent(requiementsDetailPanel);
-		requiementsDetailPanel.setLayout(new BorderLayout(0, 0));
-
-		JLabel lblNewLabel = new JLabel("Requirement Detail:");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblNewLabel.setVerticalAlignment(SwingConstants.TOP);
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		requiementsDetailPanel.add(lblNewLabel);
-
-		JPanel panel = new JPanel();
-		requiementsDetailPanel.add(panel, BorderLayout.SOUTH);
-
-		JLabel lblEstimate = new JLabel("Estimate:");
-		panel.add(lblEstimate);
-
-		textField = new JTextField();
-		panel.add(textField);
-		textField.setColumns(10);
-
-		JButton btnSubmit = new JButton("Submit");
-		btnSubmit.addActionListener(new AddVoteController(this));
-		panel.add(btnSubmit);
-
-		
-		JButton btnGetAllVotes = new JButton("Get All Votes");
-		btnGetAllVotes.addActionListener(new RetrieveAllPlanningPokerVoteController(this, new PlanningPokerRequirement()));
-		panel.add(btnGetAllVotes);
 		
 		
 		
 		setLeftComponent(LeftPanel);
-		setRightComponent(RightPanel);
+		
+		JSplitPane splitTopBottom = new JSplitPane();
+		splitTopBottom.setResizeWeight(0.8);
+		splitTopBottom.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		setRightComponent(splitTopBottom);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		splitTopBottom.setRightComponent(tabbedPane);
+		
+		JPanel statsTab = new JPanel();
+		tabbedPane.addTab("Statistics", null, statsTab, null);
+		
+		JPanel voteTab = new JPanel();
+		tabbedPane.addTab("Voting", null, voteTab, null);
+		
+		JSplitPane splitLeftRight = new JSplitPane();
+		splitLeftRight.setResizeWeight(0.8);
+		splitTopBottom.setLeftComponent(splitLeftRight);
+		
+		JPanel reqsView = new JPanel();
+		splitLeftRight.setLeftComponent(reqsView);
+		
+		JList reqsList = new JList();
+		reqsView.add(reqsList);
+		
+		JPanel ReqsDetail = new JPanel();
+		splitLeftRight.setRightComponent(ReqsDetail);
+		ReqsDetail.setLayout(new BorderLayout(0, 0));
+		
+		JList list = new JList();
+		ReqsDetail.add(list, BorderLayout.CENTER);
+		
+		JLabel lblRequirementDetail = new JLabel("Requirement Detail:");
+		lblRequirementDetail.setHorizontalAlignment(SwingConstants.CENTER);
+		ReqsDetail.add(lblRequirementDetail, BorderLayout.NORTH);
 	}
-
+/*
 	public void receiveVotes(PlanningPokerVote[] votes) {
 		String text = "";
 		for(PlanningPokerVote v: votes){
@@ -121,5 +121,5 @@ public class SessionInProgressPanel extends JSplitPane {
 	public void setTextField(String t) {
 		this.textField.setText(t);
 	}
-	
+	*/
 }

commit e9556c6c34b54c4d853c8696cb6bef09277718ed

Fixes build issue introduced in d099860f3b78ccef1facac987a1bf3703e75f5e6.

Core/WPISuite/.classpath
@@ -22,5 +22,6 @@
 	<classpathentry kind="lib" path="/RequirementManager/src"/>
 	<classpathentry kind="lib" path="/WPISuite-Interfaces/src"/>
 	<classpathentry kind="lib" path="/PlanningPoker/src"/>
+	<classpathentry combineaccessrules="false" kind="src" path="/PlanningPoker"/>
 	<classpathentry kind="output" path="bin"/>
 </classpath>

Core/WPISuite/src/edu/wpi/cs/wpisuitetng/ManagerLayer.java
@@ -35,7 +35,7 @@ import edu.wpi.cs.wpisuitetng.modules.core.entitymanagers.UserManager;
 import edu.wpi.cs.wpisuitetng.modules.core.models.User;
 import edu.wpi.cs.wpisuitetng.modules.defecttracker.entitymanagers.CommentManager;
 import edu.wpi.cs.wpisuitetng.modules.defecttracker.entitymanagers.DefectManager;
-//import edu.wpi.cs.wpisuitetng.modules.postboard.model.PostBoardEntityManager;
+import edu.wpi.cs.wpisuitetng.modules.postboard.model.PostBoardEntityManager;
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.RequirementEntityManager;
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.iterations.IterationEntityManager;
 
@@ -75,7 +75,7 @@ public class ManagerLayer {
 		map.put("coreuser", new UserManager(data));
 		map.put("defecttrackerdefect", new DefectManager(data));
 		map.put("defecttrackercomment", new CommentManager(data));
-//		map.put("postboardpostboardmessage", new PostBoardEntityManager(data));
+		map.put("postboardpostboardmessage", new PostBoardEntityManager(data));
 		map.put("requirementmanager" + "requirement", new RequirementEntityManager(data));
 		map.put("requirementmanager" + "iteration", new IterationEntityManager(data));
 		map.put("planningpoker" + "session", new PlanningPokerSessionEntityManager(data));	

PlanningPoker/.classpath
@@ -5,7 +5,6 @@
 	<classpathentry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-1.7"/>
 	<classpathentry kind="src" path="/Janeway"/>
 	<classpathentry combineaccessrules="false" kind="src" path="/Network"/>
-	<classpathentry combineaccessrules="false" kind="src" path="/WPISuite"/>
 	<classpathentry combineaccessrules="false" kind="src" path="/WPISuite-Interfaces"/>
 	<classpathentry combineaccessrules="false" kind="src" path="/RequirementManager"/>
 	<classpathentry combineaccessrules="false" kind="src" path="/DefectTracker"/>

PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/overviews/SessionInProgressPanel.java
@@ -28,6 +28,7 @@ public class SessionInProgressPanel extends JSplitPane {
 	private	 JLabel name;
 	private	 JLabel description;
 	private JLabel deadline;
+	private JTextField textField;
 
 	/**
 	 * Create the panel.
@@ -169,7 +170,6 @@ public class SessionInProgressPanel extends JSplitPane {
 		return Integer.getInteger(vote.toString());
 	}
 	
-/*
 	public void receiveVotes(PlanningPokerVote[] votes) {
 		String text = "";
 		for(PlanningPokerVote v: votes){
@@ -178,9 +178,12 @@ public class SessionInProgressPanel extends JSplitPane {
 		//System.out.println("Recieved votes: " + text);
 		this.textField.setText(text);
 	}
+	
+	public JTextField getTextField() {
+  		return textField;
+  	}
 
 	public void setTextField(String t) {
 		this.textField.setText(t);
 	}
-	*/
 }