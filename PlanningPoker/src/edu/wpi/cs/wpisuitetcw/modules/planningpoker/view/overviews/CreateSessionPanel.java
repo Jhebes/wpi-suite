02c3ff5da3fd983bef4399d491f176a7cfb9bcce
Robert McKenna <remckenna@wpi.edu>
2014-04-06 15:23:03 -0400

    Makes it so when the user chooses not to set a deadline, no deadline will show up in the deadline field of the overview tab.

--- PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/overviews/CreateSessionPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/overviews/CreateSessionPanel.java
@@ -61,7 +61,7 @@ public class CreateSessionPanel extends JSplitPane {
 
 	private JLabel labelName;
 	private final JTextArea descriptionBox;
-
+	// check box for enabling date and time deadline.
 	private JCheckBox cbDeadline;
 
 	/** Model used for requirements JList */
@@ -193,22 +193,30 @@ public class CreateSessionPanel extends JSplitPane {
 	 * @return date for the deadline object
 	 */
 	public Date getDeadline() {
-		Date date = deadlinePicker.getDate();
-		Date time = (Date) pickerDeadlineTime.getValue();
-		Calendar calendar1 = new GregorianCalendar();
-		Calendar calendar2 = new GregorianCalendar();
+		//checks to see if the deadline picker is enabled, if it is return a deadline.
+		if (this.deadlinePicker.isEnabled()) {
+			Date date = deadlinePicker.getDate();
+			Date time = (Date) pickerDeadlineTime.getValue();
+			Calendar calendar1 = new GregorianCalendar();
+			Calendar calendar2 = new GregorianCalendar();
 
-		calendar1.setTime(date);
-		calendar2.setTime(time);
+			calendar1.setTime(date);
+			calendar2.setTime(time);
 
-		// adding the time to date object
-		calendar1
-				.set(Calendar.HOUR_OF_DAY, calendar2.get(Calendar.HOUR_OF_DAY));
-		calendar1.set(Calendar.MINUTE, calendar2.get(Calendar.MINUTE));
-		calendar1.set(Calendar.SECOND, calendar2.get(Calendar.SECOND));
+			// adding the time to date object
+			calendar1.set(Calendar.HOUR_OF_DAY,
+					calendar2.get(Calendar.HOUR_OF_DAY));
+			calendar1.set(Calendar.MINUTE, calendar2.get(Calendar.MINUTE));
+			calendar1.set(Calendar.SECOND, calendar2.get(Calendar.SECOND));
 
-		Date deadline = calendar1.getTime();
-		return deadline;
+			Date deadline = calendar1.getTime();
+
+			return deadline;
+		} else {
+			// if the deadline picker isn't enabled don't return a deadline.
+			Date deadline = null;
+			return deadline;
+		}
 	}
 
 	/**
@@ -313,7 +321,7 @@ public class CreateSessionPanel extends JSplitPane {
 		this.deadlinePicker.setEnabled(true);
 		this.pickerDeadlineTime.setEnabled(true);
 	}
-	
+
 	/**
 	 * Disable the deadline picker and remote the data
 	 */
