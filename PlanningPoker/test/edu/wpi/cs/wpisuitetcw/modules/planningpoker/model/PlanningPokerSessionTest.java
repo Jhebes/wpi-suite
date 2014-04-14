3bd2fe8fda0f5571e35f44806e161d7c7cdd33fb
Hoang Ngo <hoangngo.dev@gmail.com>
2014-04-13 20:20:26 -0400

    Write test cases for PlanningPokerSession's methods

--- PlanningPoker/test/edu/wpi/cs/wpisuitetcw/modules/planningpoker/model/PlanningPokerSessionTest.java
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetcw/modules/planningpoker/model/PlanningPokerSessionTest.java
@@ -11,6 +11,54 @@
 
 package edu.wpi.cs.wpisuitetcw.modules.planningpoker.model;
 
-public class PlanningPokerSessionTest {
+import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.wpi.cs.wpisuitetcw.modules.planningpoker.models.PlanningPokerSession;
+
+public class PlanningPokerSessionTest {
+	private PlanningPokerSession planningPokerSession;
+	
+	@Before
+	public void setUp() {
+		planningPokerSession = new PlanningPokerSession();
+		
+	}
+	
+	@Test
+	public void testActivate() {		
+		// Activate a session that is not cancelled and not active. [Default case] Expected true
+		planningPokerSession.activate();
+		// Activate a session that is cancelled and not active. Expected false
+		// Activate a session that is not cancelled and active. Expected false
+		// Activate a session that is cancelled and active. Expected false
+	}
+	
+	@Test
+	public void testCancel() {
+		planningPokerSession = new PlanningPokerSession();
+		planningPokerSession.cancel();
+		assertTrue(planningPokerSession.isCancelled());
+		// End time is not null anymore. Cannot test this
+	}
+	
+	@Test
+	public void testGetStatus() {
+		planningPokerSession = new PlanningPokerSession();
+		// Test new case. [Default case] Expected "New"
+		assertEquals("New", planningPokerSession.getStatus());
+		
+		// Test cancel case. Expected "Cancelled"
+		planningPokerSession.cancel();
+		assertEquals("Cancelled", planningPokerSession.getStatus());
+		
+		// Test open case. Expected "Open"
+		planningPokerSession.activate();
+		assertEquals("Open", planningPokerSession.getStatus());
+		
+		// Test closed case. Expected "Closed"
+		assertEquals("Closed", planningPokerSession.getStatus());
+	}
 }
