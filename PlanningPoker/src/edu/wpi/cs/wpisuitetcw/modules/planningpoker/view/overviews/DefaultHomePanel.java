4dd9b14118926c8cb16512e2a135e4bbc2771497
Jenny <jalbores@wpi.edu>
2014-04-15 20:08:31 -0400

    formats home page. Jenny

--- PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/overviews/DefaultHomePanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/overviews/DefaultHomePanel.java
@@ -3,60 +3,71 @@
  */
 package edu.wpi.cs.wpisuitetcw.modules.planningpoker.view.overviews;
 
+import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-import javax.swing.border.EmptyBorder;
-
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.BasicStroke;
-
-
 
 /**
  * @author Jenny12593
- *
+ * 
  */
-public class DefaultHomePanel extends JPanel{
+public class DefaultHomePanel extends JPanel {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private final String CREATESESSIONSTRING = "Create Session.";
 	private final String OPENSESSIONSTRING = "Open Sessions.";
 	private final String ALLSESSIONSSTRING = "All Sessions.";
-	private final String WELCOMESTRING = "Welcome to Planning Poker! "
-			+ "To get started, click ".concat(CREATESESSIONSTRING)			
-			+ " To view open sessions click on " .concat(OPENSESSIONSTRING)
-			+ " To view all sessions click on " .concat(ALLSESSIONSSTRING);
+	private final String CLOSEDSESSIONSTRING = "Closed Sessions";
+	private final String WELCOMESTRING = "Welcome to Planning Poker!\n"
+			+ "To get started, click ".concat(CREATESESSIONSTRING)
+			+ "\nTo view all the sessions click on ".concat(ALLSESSIONSSTRING)
+			+ "\nTo view open sessions click on ".concat(OPENSESSIONSTRING)
+			+ "\nTo view closed sessions click on ".concat(CLOSEDSESSIONSTRING);
 
-	public DefaultHomePanel(){
-				
+	public DefaultHomePanel() {
+
 		// adds 10 pixels of padding
-		this.setBorder(new EmptyBorder(10, 10, 10, 10));
-//		this.createStrokeBorder(BasicStroke stroke, Paint paint)
-		
-//		try {
-//		     GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-//		     ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("A.ttf")));
-//		} catch (IOException|FontFormatException e) {
-//		     //Handle exception
-//		}
-		
+		// this.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+		// sets the panel border
+		this.setBorder(BorderFactory.createEtchedBorder(Color.lightGray,
+				Color.blue));
+
+		// this.createStrokeBorder(BasicStroke stroke, Paint paint)
+
+		// try {
+		// GraphicsEnvironment ge =
+		// GraphicsEnvironment.getLocalGraphicsEnvironment();
+		// ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new
+		// File("A.ttf")));
+		// } catch (IOException|FontFormatException e) {
+		// //Handle exception
+		// }
+
 		// Welcome text area
-		JTextArea welcomeTextArea = new JTextArea(5, 15);
-		
+		JTextArea welcomeTextArea = new JTextArea(5, 30);
+
 		Font plainFont = new Font("Serif", Font.PLAIN, 36);
 		Font boldFont = new Font("Serif", Font.BOLD, 36);
+
+		// text color
 		welcomeTextArea.setForeground(new Color(0x061692));
-		
-//		textAreaNewSessions.setFont(boldFont);
-//		textAreaNewSessions.setText(NEWSESSIONSTRING);
-//		textAreaNewSessions.setWrapStyleWord(true);
-//		textAreaNewSessions.setLineWrap(true);
-//		textAreaNewSessions.setBorder(BorderFactory.createEmptyBorder());
-//		textAreaNewSessions.setOpaque(false);
-//		textAreaNewSessions.setFocusable(false);
-//		textAreaNewSessions.setEditable(false);
-		
+
+		// textAreaNewSessions.setFont(boldFont);
+		// textAreaNewSessions.setText(NEWSESSIONSTRING);
+		// textAreaNewSessions.setWrapStyleWord(true);
+		// textAreaNewSessions.setLineWrap(true);
+		// textAreaNewSessions.setBorder(BorderFactory.createEmptyBorder());
+		// textAreaNewSessions.setOpaque(false);
+		// textAreaNewSessions.setFocusable(false);
+		// textAreaNewSessions.setEditable(false);
+
 		welcomeTextArea.setFont(plainFont);
 		welcomeTextArea.setText(WELCOMESTRING);
 		welcomeTextArea.setWrapStyleWord(true);
@@ -65,9 +76,8 @@ public class DefaultHomePanel extends JPanel{
 		welcomeTextArea.setOpaque(false);
 		welcomeTextArea.setFocusable(false);
 		welcomeTextArea.setEditable(false);
-		
-		// add the label to the home panel
+
+		// add the text area to the home panel
 		this.add(welcomeTextArea);
-//		this.add(textAreaNewSessions);
 	}
 }
