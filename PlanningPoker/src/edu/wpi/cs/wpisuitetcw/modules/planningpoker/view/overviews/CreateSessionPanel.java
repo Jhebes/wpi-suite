8736d712b54304d7f4c823f59f61c34390aaa8f7
Troy Ling <troyling14@gmail.com>
2014-04-17 20:34:33 -0400

    adds required message to create session panel.

--- PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/overviews/CreateSessionPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/overviews/CreateSessionPanel.java
@@ -56,6 +56,7 @@ public class CreateSessionPanel extends JPanel {
 	private static final int DESCRIPTION_BOX_WIDTH = 400;
 	private static final int DESCRIPTION_BOX_HEIGHT = 110;
 	private static final int GAP_LENGTH_DEADLINE_TO_BOTTOM = 220;
+	private static final String REQUIRED_LABEL = "<html><font color='red'>Required field *</font></html>";
 
 	// default data size for database entry
 	private final int DEFAULT_DATA_SIZE = 30;
@@ -73,6 +74,9 @@ public class CreateSessionPanel extends JPanel {
 
 	/** Button to cancel making a session */
 	private final JButton btnCancel;
+	
+	/** Require field label */
+	private final JLabel labelRequireField;
 
 	// ################ UI Right Component #################
 	private final CreateNewDeckPanel deckPanel;
@@ -146,6 +150,7 @@ public class CreateSessionPanel extends JPanel {
 
 		// Initialize a text box to fill a new session's name in
 		labelName = new JLabel("Name *");
+		labelRequireField = new JLabel(REQUIRED_LABEL);
 		nameTextField = new JTextField(DEFAULT_DATA_SIZE);
 
 		// Create dropdown menu to select type of session
@@ -251,6 +256,7 @@ public class CreateSessionPanel extends JPanel {
 		
 		bottomPanel.add(btnSaveSession, "left, width 120px, height 30px");
 		bottomPanel.add(btnCancel, "width 120px, height 30px");
+		bottomPanel.add(labelRequireField, "gapleft 10px");
 
 		// setup the panel
 		this.setLayout(new MigLayout());
