6e1b4ef94f1c194018bdaa1391343471f699450e
Hoang Ngo <hoangngo.dev@gmail.com>
2014-04-19 23:19:56 -0400

    Remove unnessary import

--- PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/overviews/CreateSessionPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/overviews/CreateSessionPanel.java
@@ -30,7 +30,6 @@ import javax.swing.JSpinner;
 import javax.swing.JSplitPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.ScrollPaneConstants;
 import javax.swing.SpinnerDateModel;
 
 import net.miginfocom.swing.MigLayout;
