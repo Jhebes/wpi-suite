8853425948b73ec7e9e913102b822660d8544f20
Hoang Ngo <hoangngo.dev@gmail.com>
2014-04-21 00:47:19 -0400

    Add comments to the ImportRequirementController

--- PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/controllers/ImportRequirementController.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/controllers/ImportRequirementController.java
@@ -21,18 +21,18 @@ import edu.wpi.cs.wpisuitetng.network.Request;
 import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
 
 /**
- * Creates a planning poker requirement from a requirement manager requirement
- * and sends it to the database.
+ * A controller that creates a planning poker requirement 
+ * from a requirement manager requirement and sends it to the database.
  */
 public class ImportRequirementController implements ActionListener {
-
+	/** A PlanningPokerRequirement that would be created */
 	private PlanningPokerRequirement requirement;
 	
-	public ImportRequirementController() {
-
-	}
+	public ImportRequirementController() {}
 
 	/**
+	 * Convert all Requirements to PlanningPokerRequirements and
+	 * send them to the database
 	 * {@inheritDoc}
 	 */
 	@Override
@@ -49,6 +49,12 @@ public class ImportRequirementController implements ActionListener {
 		}
 	}
 
+	/**
+	 * Send a request to get all the PlanningPokerSession of ID 1 
+	 * from the database
+	 * @param requirement A PlanningPokerRequirement that would be assigned
+	 * to the ImportRequirementController
+	 */
 	public void importRequirement(PlanningPokerRequirement requirement) {
 		this.requirement = requirement;
 		final Request request = Network.getInstance().makeRequest(
@@ -57,7 +63,12 @@ public class ImportRequirementController implements ActionListener {
 		request.send();
 	}
 
-	
+	/**
+	 * Add the requirement to the PlanningPokerSession.
+	 * This method is called by the ImportRequirementRequestObserver
+	 * @param freeReqsSession A session that would have the 
+	 * PlanningPokerRequirement stored in this controller
+	 */
 	public void onSuccess(PlanningPokerSession freeReqsSession) {
 		freeReqsSession.addRequirement(requirement);
 		freeReqsSession.save();
