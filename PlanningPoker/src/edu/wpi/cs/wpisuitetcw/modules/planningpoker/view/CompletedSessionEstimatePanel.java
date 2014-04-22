272b553b224fa074bd1148868296d0215b0c8235
Robert McKenna <remckenna@wpi.edu>
2014-04-22 02:02:54 -0400

    Adds field and button for submitting final estimate. These don't work.

--- PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/CompletedSessionEstimatePanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/CompletedSessionEstimatePanel.java
@@ -18,7 +18,9 @@ import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -27,7 +29,7 @@ import javax.swing.JTextField;
 
 public class CompletedSessionEstimatePanel extends JPanel {
 
-	//private final JPanel pnlCompletedSession;
+	// private final JPanel pnlCompletedSession;
 	private final GridLayout panelLayout;
 	private final JPanel pnlFinalEstimate;
 	private final JPanel pnlStats;
@@ -42,115 +44,133 @@ public class CompletedSessionEstimatePanel extends JPanel {
 	private JTextField statsMean;
 	private JTextField statsMedian;
 	private JTextField statsMode;
+	private JTextField finalEstimateField;
 	private final Font headerFont;
+	private final Font statNameFont;
+	private final JButton btnFinalEstimate;
+
 
 	/**
 	 * Create the panel.
 	 */
 	public CompletedSessionEstimatePanel() {
-		
+
 		panelLayout = new GridLayout(0, 3);
 
 		/*
-		 * Create and set up the 3 panels used to 
-		 * create the CompletedSession Panel
+		 * Create and set up the 3 panels used to create the CompletedSession
+		 * Panel
 		 */
 		// Used to make the final estimate
 		this.setLayout(panelLayout);
 		pnlFinalEstimate = new JPanel();
-		pnlFinalEstimate.setLayout(new BoxLayout(pnlFinalEstimate, BoxLayout.Y_AXIS));
-		pnlFinalEstimate.setBorder(BorderFactory.createLineBorder(Color.orange));
-		
+		pnlFinalEstimate.setLayout(new BoxLayout(pnlFinalEstimate,
+				BoxLayout.Y_AXIS));
+		pnlFinalEstimate
+				.setBorder(BorderFactory.createLineBorder(Color.orange));
+
 		// Statistical info of the PP Session
-		pnlStats = new JPanel(); 
+		pnlStats = new JPanel();
 		pnlStats.setLayout(new BoxLayout(pnlStats, BoxLayout.Y_AXIS));
 		pnlStats.setBorder(BorderFactory.createLineBorder(Color.red));
-		
+
 		// Table of votes for each req
-		pnlVotes = new JPanel(); 
+		pnlVotes = new JPanel();
 		pnlVotes.setLayout(new BoxLayout(pnlVotes, BoxLayout.Y_AXIS));
 		pnlVotes.setBorder(BorderFactory.createLineBorder(Color.green));
-		
-		
 
 		// Initialize the default font for JLabel headers
-		headerFont = new Font("TimesRoman", Font.BOLD, 20);
+		headerFont = new Font("TimesRoman", Font.BOLD, 25);
 
 		// Initialize the Headers for the panels.
 		lblVotes = new JLabel("Votes");
 		lblVotes.setFont(headerFont);
 		lblVotes.setAlignmentX(Component.CENTER_ALIGNMENT);
-		
+
 		lblStats = new JLabel("Stats");
 		lblStats.setFont(headerFont);
 		lblStats.setAlignmentX(Component.CENTER_ALIGNMENT);
-		
+
 		lblFinalEstimate = new JLabel("Final Estimate");
 		lblFinalEstimate.setFont(headerFont);
 		lblFinalEstimate.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+		
+		
+		
+		
 		// Create the Final Estimate Panel
+		finalEstimateField = new JTextField(3);
+		finalEstimateField.setFont(new Font("TimeRoman", Font.BOLD, 30));
+		finalEstimateField.setMaximumSize(finalEstimateField.getPreferredSize());
+		finalEstimateField.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
+		Component verticalStrut = Box.createVerticalStrut(50);
+		
+		
+		btnFinalEstimate = new JButton("Submit Final Estimation");
+		btnFinalEstimate.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
 		pnlFinalEstimate.add(lblFinalEstimate);
+		pnlFinalEstimate.add(verticalStrut);
+		pnlFinalEstimate.add(finalEstimateField);
+		pnlFinalEstimate.add(btnFinalEstimate);
+		
 
-		
-		
-		
 		// Create the Stats Panel
+		statNameFont = new Font("TimesRoman", Font.BOLD, 15);
+		
 		lblMean = new JLabel("Mean");
+		lblMean.setFont(statNameFont);
 		lblMean.setAlignmentX(Component.CENTER_ALIGNMENT);
 		statsMean = new JTextField(10);
 		// Keeps the text box from filling the entire statBox
-		statsMean.setMaximumSize(statsMean.getPreferredSize()); 
+		statsMean.setMaximumSize(statsMean.getPreferredSize());
 		statsMean.setAlignmentX(Component.CENTER_ALIGNMENT);
 		statsMean.setEditable(false);
-		
+
 		lblMedian = new JLabel("Median");
+		lblMedian.setFont(statNameFont);
 		lblMedian.setAlignmentX(Component.CENTER_ALIGNMENT);
 		statsMedian = new JTextField(10);
 		// Keeps the text box from filling the entire statBox
 		statsMedian.setMaximumSize(statsMedian.getPreferredSize());
 		statsMedian.setAlignmentX(Component.CENTER_ALIGNMENT);
 		statsMedian.setEditable(false);
-		
+
 		lblMode = new JLabel("Mode");
+		lblMode.setFont(statNameFont);
 		lblMode.setAlignmentX(Component.CENTER_ALIGNMENT);
 		statsMode = new JTextField(10);
 		// Keeps the text box from filling the entire statBox
-		statsMode.setMaximumSize(statsMode.getPreferredSize()); 
+		statsMode.setMaximumSize(statsMode.getPreferredSize());
 		statsMode.setAlignmentX(Component.CENTER_ALIGNMENT);
 		statsMode.setEditable(false);
-		
+
 		pnlStats.add(lblStats);
-		
+
 		pnlStats.add(lblMean);
 		pnlStats.add(statsMean);
-		
+
 		pnlStats.add(lblMedian);
 		pnlStats.add(statsMedian);
-		
+
 		pnlStats.add(lblMode);
 		pnlStats.add(statsMode);
-		
 
-	
-		
-		
-		
 		// Create the Votes Panel
-		Object[][] data = {
-				{"remckenna",new Integer(2)}, /// This data is all dummy change
-				{"Somebody else", new Integer(6)}
-				};
-		Object[] voteTableColHeaders = {"User", "Votes"};
+		Object[][] data = { { "remckenna", new Integer(2) }, // / This data is
+																// all dummy
+																//TODO: change
+				{ "Somebody else", new Integer(6) } };
+		Object[] voteTableColHeaders = { "User", "Vote" };
 		tblVotes = new JTable(data, voteTableColHeaders);
-		
+
 		JScrollPane votesScrollPane = new JScrollPane(tblVotes);
 		tblVotes.setFillsViewportHeight(true);
-		
+
 		pnlVotes.add(lblVotes);
 		pnlVotes.add(votesScrollPane);
-		
-		
 
 		// put the completed Session panel together
 		this.add(pnlVotes);
@@ -158,24 +178,27 @@ public class CompletedSessionEstimatePanel extends JPanel {
 		this.add(pnlFinalEstimate);
 
 	}
-	
 
-
+	/**
+	 * sets the text field for Mean in the completed session view.
+	 * @param statsMean
+	 */
 	public void setStatsMean(int statsMean) {
-		this.statsMean.setText(""+ statsMean +"  ;");
+		this.statsMean.setText("" + statsMean + "  ;");
 	}
-
-
-
+	/**
+	 * sets the text field for Median in the completed session view.
+	 * @param statsMedian
+	 */
 	public void setStatsMedian(int statsMedian) {
 		this.statsMedian.setText("" + statsMedian + "  ;");
 	}
-
-
-
+	/**
+	 * sets the text field for Mode in the completed session view.
+	 * @param statsMode
+	 */
 	public void setStatsMode(int statsMode) {
-		this.statsMode.setText(""+ statsMode + "  ;");
+		this.statsMode.setText("" + statsMode + "  ;");
 	}
-	
-	
+
 }
