54dd5756ddb319ddcc1ce49a4b26320ddbac0b4c
Troy Ling <troyling14@gmail.com>
2014-04-22 01:11:01 -0400

    creates panel in support of displaying a deck of cards for voting.

--- PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/models/PlanningPokerDeck.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/models/PlanningPokerDeck.java
@@ -17,43 +17,46 @@ import com.google.gson.Gson;
 import edu.wpi.cs.wpisuitetng.modules.AbstractModel;
 
 /**
- * This class represents the deck, a list of cards with values
- * PlanningPokerDeck provides cards with specified values so that
- * the users can vote on the requirements
+ * This class represents the deck, a list of cards with values PlanningPokerDeck
+ * provides cards with specified values so that the users can vote on the
+ * requirements
  */
 public class PlanningPokerDeck extends AbstractModel {
 	/** List of card values */
 	private ArrayList<Integer> deck;
-	
+
 	/** Name of the deck */
 	private String deckName;
 
 	/** ID of the deck */
 	private int id;
-	
+
 	/**
-	 * Construct the default deck in which the cards have Fibonacci values
-	 * TODO the default deck should be a database entry
+	 * Construct the default deck in which the cards have Fibonacci values TODO
+	 * the default deck should be a database entry
 	 */
 	public PlanningPokerDeck() {
-		int[] defaultDeck = {0,1,1,2,3,5,8,13};
+		int[] defaultDeck = { 0, 1, 1, 2, 3, 5, 8, 13 };
 		this.deckName = "Default Deck";
 		deck = new ArrayList<Integer>();
-		for(int i : defaultDeck) {
+		for (int i : defaultDeck) {
 			this.deck.add(i);
 		}
 	}
-	
+
 	/**
 	 * Construct a deck from the given name and arrayList of values
-	 * @param deckName The name of the deck
-	 * @param deck An array of integer representing the cards' values
+	 * 
+	 * @param deckName
+	 *            The name of the deck
+	 * @param deck
+	 *            An array of integer representing the cards' values
 	 */
-	public PlanningPokerDeck(String deckName, ArrayList<Integer> deck){
+	public PlanningPokerDeck(String deckName, ArrayList<Integer> deck) {
 		this.deckName = deckName;
 		this.deck = deck;
 	}
-	
+
 	/**
 	 * Returns an array of PlanningPokerSession parsed from the given
 	 * JSON-encoded string.
@@ -68,7 +71,7 @@ public class PlanningPokerDeck extends AbstractModel {
 		final Gson parser = new Gson();
 		return parser.fromJson(json, PlanningPokerDeck[].class);
 	}
-	
+
 	/**
 	 * Returns an instance of PlanningPokerRequirement constructed using the
 	 * given Requirement encoded as a JSON string.
@@ -82,46 +85,49 @@ public class PlanningPokerDeck extends AbstractModel {
 		final Gson parser = new Gson();
 		return parser.fromJson(json, PlanningPokerDeck.class);
 	}
-	
+
 	/**
-	 * {@inheritDoc}}
+	 * {@inheritDoc}
 	 */
 	@Override
 	public String toJSON() {
 		return new Gson().toJson(this, PlanningPokerDeck.class);
 	}
-	
-	/**
-	 * This class does not provide implementation for this method
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void save() {}
 
 	/**
-	 * This class does not provide implementation for this method
-	 * {@inheritDoc}
+	 * This class does not provide implementation for this method {@inheritDoc}
 	 */
 	@Override
-	public void delete() {}
-	
+	public void save() {
+	}
+
 	/**
-	 * This class does not provide implementation for this method
-	 * {@inheritDoc}
+	 * This class does not provide implementation for this method {@inheritDoc}
 	 */
 	@Override
-	public Boolean identify(Object o) {return null;}
+	public void delete() {
+	}
+
+	/**
+	 * This class does not provide implementation for this method {@inheritDoc}
+	 */
+	@Override
+	public Boolean identify(Object o) {
+		return null;
+	}
 
 	/**
 	 * Return the name of the deck
+	 * 
 	 * @return Return the name of the deck
 	 */
 	public String getDeckName() {
 		return deckName;
 	}
-	
+
 	/**
 	 * Return the ID of the deck
+	 * 
 	 * @return Return the ID of the deck
 	 */
 	public int getId() {
@@ -130,10 +136,20 @@ public class PlanningPokerDeck extends AbstractModel {
 
 	/**
 	 * Assign an integer to the ID of the deck
-	 * @param id An integer that would be assigned to the deck's ID
+	 * 
+	 * @param id
+	 *            An integer that would be assigned to the deck's ID
 	 */
 	public void setId(int id) {
 		this.id = id;
+	}
+	
+	/**
+	 * 
+	 * @return the deck list
+	 */
+	public ArrayList<Integer> getDeck() {
+		return deck;
 	}
 
 }
---
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/pokers/DispalyDeckPanel.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2014 WPI-Suite
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Team Combat Wombat
+ ******************************************************************************/
+
+package edu.wpi.cs.wpisuitetcw.modules.planningpoker.view.pokers;
+
+import javax.swing.JPanel;
+
+import edu.wpi.cs.wpisuitetcw.modules.planningpoker.models.PlanningPokerDeck;
+
+public class DispalyDeckPanel extends JPanel {
+	private final PlanningPokerDeck deck;
+	
+	public DispalyDeckPanel(PlanningPokerDeck deck) {
+		this.deck = deck;
+		displayDeck();
+	}
+	
+	
+	/**
+	 * Display a deck of cards for voting.
+	 */
+	public void displayDeck() {
+		for(int value : this.deck.getDeck()) {
+			Card aCard = new Card();
+		}
+	}
+	
+}
