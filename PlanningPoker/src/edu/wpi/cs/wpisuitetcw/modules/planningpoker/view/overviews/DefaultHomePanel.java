1fe2e47384df8082bc26690a9206ec6e43c96f48
Jenny <jalbores@wpi.edu>
2014-04-23 01:54:48 -0400

    Finishes the default home panel.

--- PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/overviews/DefaultHomePanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/overviews/DefaultHomePanel.java
@@ -13,6 +13,7 @@ package edu.wpi.cs.wpisuitetcw.modules.planningpoker.view.overviews;
 import java.awt.Color;
 
 import javax.swing.BorderFactory;
+import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 import javax.swing.JTextPane;
 import javax.swing.border.Border;
@@ -22,11 +23,13 @@ import javax.swing.text.StyleConstants;
 import javax.swing.text.StyleContext;
 import javax.swing.text.StyledDocument;
 
+import net.miginfocom.swing.MigLayout;
+
 /**
  * @author Jenny12593
  * 
  */
-public class DefaultHomePanel extends JSplitPane {
+public class DefaultHomePanel extends JPanel {
 
 	/**
 	 * 
@@ -55,19 +58,30 @@ public class DefaultHomePanel extends JSplitPane {
 
 		JTextPane leftTextPane = createLeftTextPane();
 		JTextPane rightTextPane = createRightTextPane();
+		JTextPane topTextPane = createTopTextPane();
 
 		leftTextPane.setEditable(false);
 		rightTextPane.setEditable(false);
-
+		topTextPane.setEditable(false);
+		
+		JPanel topContainer = new JPanel();
+		JSplitPane container = new JSplitPane();
+		
+		container.setBorder(null);
+	
 		// setup the layout
-		this.setLeftComponent(leftTextPane);
-		this.setRightComponent(rightTextPane);
-		this.setDividerSize(0);
-		this.setResizeWeight(.5);
-		this.setEnabled(true);
+		
+		container.setLeftComponent(leftTextPane);
+		container.setRightComponent(rightTextPane);
+		container.setDividerSize(0);
+		container.setResizeWeight(.5);
+		container.setEnabled(true);
 
+		topContainer.add(topTextPane);		
+		
 		leftTextPane.setOpaque(false);
 		rightTextPane.setOpaque(false);
+		topTextPane.setOpaque(false);
 
 		// //set sizes of JTextPanes
 		// leftTextPane.setBounds( 100, 100, 50, 50 );
@@ -76,7 +90,7 @@ public class DefaultHomePanel extends JSplitPane {
 		//
 		// this.setPreferredSize(new Dimension( 600, 600 ));
 
-		// // Adds left and right Panes in Flow Layout
+		 // Adds left and right Panes in Flow Layout
 		// this.add(leftTextPane);
 		// this.add(rightTextPane);
 
@@ -92,6 +106,10 @@ public class DefaultHomePanel extends JSplitPane {
 
 		// sets the panel border
 		this.setBorder(compound);
+		
+		this.setLayout(new MigLayout());
+		this.add(topContainer, "dock north");
+		this.add(container);
 
 	}
 
@@ -115,28 +133,51 @@ public class DefaultHomePanel extends JSplitPane {
 
 	// add the text area to the home panel
 	// this.add(welcomeTextBox);
+	
+	private JTextPane createTopTextPane() {
+		String[] TopPaneText = {"Welcome to Planning Poker!"			
+		                   };
+		
+		String[] initStyles = {"super huge"}; 
+
+		JTextPane textPane = new JTextPane();
+		StyledDocument doc = textPane.getStyledDocument();
+		addStylesToDocument(doc);
+
+		try {
+			for (int i = 0; i < TopPaneText.length; i++) {
+				doc.insertString(doc.getLength(), TopPaneText[i],
+						doc.getStyle(initStyles[i]));
+			}
+		} catch (BadLocationException ble) {
+			System.err.println("Couldn't insert initial text into text pane.");
+		}
+
+		return textPane;
+	}
+
 
 	private JTextPane createLeftTextPane() {
 
 		String[] leftPaneText = {
-				"       Welcome to Planning Poker!", // super huge
-				"\n\n" + "What is Planning Poker?", // large
-				"\n   Planning Poker is a software development tool for estimating "
-						+ "requirements in software development projects. In Planning Poker,"
+				"What is Planning Poker?", // large
+				"\n\n   Planning Poker is a software development tool for estimating "
+						+ "requirements in software development projects. In Planning Poker, "
 						+ "each group member has a deck of cards to rank the requirements "
 						+ "of a project according to how important they think that task "
 						+ "ranks among the others. The project administrators can then "
 						+ "use this data to create a final priority list for the project."
-						+ "", // small
-						"\nHow To Get Started",      // large
-						"\n   To start a planning poker session, click create a session and "
+						+ "\n", // small
+				"\nHow To Get Started", // large
+				"\n   To start a planning poker session, click create a session and "
 						+ "assign a type of deck to the session. You can either use the "
 						+ "default deck, or create your own deck. Once a session has been created, "
 						+ "you can select what requirements you would like to add to the session "
-						+ "or create a requirement of your own. Now it's time to start the voting!"
-		};
+						+ "or create a requirement of your own. Now it's time to start the voting!\n", // small
+				 };
 
-		String[] initStyles = { "super huge", "large", "small", "large", "small"};
+		String[] initStyles = {"large", "small", "large",
+				"small" };
 
 		JTextPane textPane = new JTextPane();
 		StyledDocument doc = textPane.getStyledDocument();
@@ -156,12 +197,22 @@ public class DefaultHomePanel extends JSplitPane {
 
 	private JTextPane createRightTextPane() {
 
-		String[] rightPaneText = { " ", // super huge
-				"\n\nThis will be filled with questions", // large
-				"\nYo, add more stuff" // regular
+		String[] rightPaneText = {
+				"Frequently Asked Questions", // large
+				"\n\nWho can view my vote?", // medium
+				"\n Once your vote is submitted, your vote remains anonymous and is only"
+				+ " used for the calculation of the final estimation of a requirement.", //small
+				"\n\nWhat is the difference between cancelling and ending a session?", // medium
+				"\n   When a session ends due to the deadline being reached or the administrator"
+				+ " manually ending a session, a final estimation is calculated, while cancelling "
+				+ "a session does not generate a final estimation.", //small
+				"", //medium
+				"" //small
+				
 		};
 
-		String[] initStyles = { "super huge", "large", "regular" };
+		String[] initStyles = {"large", "medium", "small", "medium", "small",
+								"medium", "small"};
 
 		JTextPane textPane = new JTextPane();
 		StyledDocument doc = textPane.getStyledDocument();
@@ -192,20 +243,25 @@ public class DefaultHomePanel extends JSplitPane {
 
 		Style s = doc.addStyle("italic", regular);
 		StyleConstants.setItalic(s, true);
-//
+		//
 		s = doc.addStyle("bold", regular);
 		StyleConstants.setBold(s, true);
 
 		s = doc.addStyle("small", regular);
 		StyleConstants.setFontSize(s, 19);
 
+		s = doc.addStyle("medium", regular);
+		StyleConstants.setFontSize(s, 20);
+		StyleConstants.setBold(s,true);
+		
 		s = doc.addStyle("large", Georgia);
 		StyleConstants.setFontSize(s, 24);
 		StyleConstants.setBold(s, true);
 
 		s = doc.addStyle("super huge", Georgia);
+		StyleConstants.setFontSize(s, 34);
 		StyleConstants.setBold(s, true);
-		StyleConstants.setFontSize(s, 30);
+
 
 		// s = doc.addStyle("icon", regular);
 		// StyleConstants.setAlignment(s, StyleConstants.ALIGN_CENTER);
@@ -218,4 +274,3 @@ public class DefaultHomePanel extends JSplitPane {
 
 	}
 }
-
