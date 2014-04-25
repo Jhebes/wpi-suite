4d690d024683289184ad34bc2b3fc50409e1b734
Troy Ling <troyling14@gmail.com>
2014-04-25 11:39:48 -0400

    creates validator for create session panel

---
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/session/SessionInputValidator.java
@@ -0,0 +1,14 @@
+package edu.wpi.cs.wpisuitetcw.modules.planningpoker.view.session;
+
+public class SessionInputValidator {
+
+	/** create session panel that needs to be validated */
+	private final CreateSessionPanel sessionPanel;
+
+	/**
+	 * Constructor initialize the session panel
+	 */
+	public SessionInputValidator(CreateSessionPanel sessionPanel) {
+		this.sessionPanel = sessionPanel;
+	}
+}
