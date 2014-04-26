9bc55f5a304456adcfb5e0810837d88bc9b73cf0
Troy Ling <troyling14@gmail.com>
2014-04-26 18:09:19 -0400

    fixes bug that cancelled session is listed as open session

--- PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/models/PlanningPokerSession.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/models/PlanningPokerSession.java
@@ -104,7 +104,7 @@ public class PlanningPokerSession extends AbstractModel {
 	 * @return Return true if the session is open
 	 */
 	public boolean isOpen() {
-		return isActive() && !isClosed();
+		return isActive() && !isClosed() && !isCancelled;
 	}
 
 	/**
