0ead85a6810c800756c7618e481d2eb93d61078d
Penny <12pover@gmail.com>
2014-04-27 17:33:17 -0400

    added auto validation to final estimate

--- PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/CompletedSessionEstimatePanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/CompletedSessionEstimatePanel.java
@@ -146,7 +146,7 @@ public class CompletedSessionEstimatePanel extends JPanel {
 
 		btnFinalEstimate = new JButton("Submit Final Estimation");
 		btnFinalEstimate.setAlignmentX(Component.CENTER_ALIGNMENT);
-		// btnFinalEstimate.setEnabled(false);
+		btnFinalEstimate.setEnabled(false);
 
 		btnFinalEstimate.addActionListener(new ActionListener() {
 
