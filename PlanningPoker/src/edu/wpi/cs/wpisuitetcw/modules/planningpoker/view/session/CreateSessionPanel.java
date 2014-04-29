Jhebes/wpi-suite

commit 360a69b57c7c7b0dfd1b4b745dd35148d5162916

Fix if statement with boolean return.

PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/session/CreateSessionPanel.java
@@ -354,11 +354,7 @@ public class CreateSessionPanel extends JPanel {
 	 */
 	private boolean isSessionNameEntered() {
 		// textbox for session name
-		if (nameTextField.getText().equals("")) {
-			return false;
-		} else {
-			return true;
-		}
+		return nameTextField.getText().equals("");
 	}
 
 	/**
@@ -368,11 +364,7 @@ public class CreateSessionPanel extends JPanel {
 	 */
 	private boolean isSessionDescriptionEntered() {
 		// textarea for session description
-		if (descriptionBox.getText().equals("")) {
-			return false;
-		} else {
-			return true;
-		}
+		return descriptionBox.getText().equals("");
 	}
 
 	/**

PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/pokers/Card.java
@@ -237,11 +237,7 @@ public class Card extends JPanel {
 	private boolean isPositiveInteger(String s) {
 		try {
 			final int value = Integer.parseInt(s);
-			if (value > 0) {
-				return true;
-			} else {
-				return false;
-			}
+			return value > 0;
 		} catch (NumberFormatException e) {
 			return false;
 		}

commit 20258bf33412c95d510fa4af10dedb12a856e591

Fixes bug introduced in 360a69b5.

PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/session/CreateSessionPanel.java
@@ -354,7 +354,7 @@ public class CreateSessionPanel extends JPanel {
 	 */
 	private boolean isSessionNameEntered() {
 		// textbox for session name
-		return nameTextField.getText().equals("");
+		return !nameTextField.getText().equals("");
 	}
 
 	/**
@@ -364,7 +364,7 @@ public class CreateSessionPanel extends JPanel {
 	 */
 	private boolean isSessionDescriptionEntered() {
 		// textarea for session description
-		return descriptionBox.getText().equals("");
+		return !descriptionBox.getText().equals("");
 	}
 
 	/**