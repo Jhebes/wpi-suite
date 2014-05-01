51c0fc82cdc0ff4b81740ba1423b1c9c70fb677f
nakalamvokis <nkal123@gmail.com>
2014-04-30 22:08:26 -0400

    Adds progress on final estimation panel nakalamvokis

--- PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/ImagePanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetcw/modules/planningpoker/view/ImagePanel.java
@@ -3,16 +3,18 @@ package edu.wpi.cs.wpisuitetcw.modules.planningpoker.view;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 class ImagePanel extends JPanel {
-
 	private Image img;
 
-	public ImagePanel(String img) {
-		this(new ImageIcon(img).getImage());
+	public ImagePanel(String img) throws IOException {
+		// this.img = new ImageIcon(img).getImage();
+		this.img = ImageIO.read(getClass().getResource("new_card.png"));
 	}
 
 	public ImagePanel(Image img) {
